413340f91fdfc0833f49c4260217f857794d82c6
Jared Hileman <156353004+Jaredhileman@users.noreply.github.com>
2024-04-26 13:31:22 -0600

    updated datagen and centering function

--- R/data_gen.R
+++ R/data_gen.R
@@ -1,5 +1,16 @@
-
-#' @export
+#' Generating simulated data
+#'
+#' Utilizes a random normal generation with mean, sd set by user. The function
+#' passes the generated data through a logistic function to output
+#' probabilities, which are then passed into the rbinom function to generate
+#' binary responses motivated by the predictor variable, X ~ N(mean, sd).
+#'
+#' @param size The number of observations to generate
+#' @param center The mean of the normal distribution
+#' @param sd The standard deviation of the normal distribution
+#' @return A data frame with columns Y and X
+#'
+#' #' @export
 data_gen <- function(size, center, sd = 1) {
   pred <- rnorm(size, center, sd)
   prob <- exp(pred) / (1 + exp(pred))
--- Report.R
+++ Report.R
@@ -1,1 +1,15 @@
+#####
+# Simulate 100 datasets of size n = 100
+
+datalist <- list()
+youden_list <- list()
+
+centered <- centering(seq(0.05, .95, by = 0.05))
+for (i in 1:5) {
+  datalist[[i]] <- data_gen(size = 100, center = centered)
+  youden_list[[i]] <- best_cutoff(datalist[[i]])
+}
+
+View(datalist)
+View(youden_list)
 
