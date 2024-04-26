528d5295598d07743c05a621be37ecfd54245876
Jared Hileman <156353004+Jaredhileman@users.noreply.github.com>
2024-04-26 12:26:37 -0600

    updated youdens J to best_cutoff
    changed youden's j function to utilize a minimizing dist / maximizing youden's J approach.

--- R/data_gen.R
+++ R/data_gen.R
@@ -1,7 +1,7 @@
 
 #' @export
-data_gen <- function(size, center) {
-  pred <- rnorm(size, center, 1)
+data_gen <- function(size, center, sd = 1) {
+  pred <- rnorm(size, center, sd)
   prob <- exp(pred) / (1 + exp(pred))
   response <- rbinom(size, 1, prob)
   data <- as.data.frame(cbind(response, pred))
--- R/youdens_J.R
+++ R/youdens_J.R
@@ -1,12 +1,14 @@
 
 #' @export
-youdens_J <- function(model, dataset) {
+best_cutoff <- function(dataset) {
+  model <- glm(Y ~ ., data = dataset, family = binomial)
   pred <- predict(model, dataset, type = "response")
   cutoff <- seq(0, 1, .05)
   lst <- list()
   lst$prediction_matrix <- lapply(cutoff,
                                   function(val) ifelse(pred > val, 1, 0))
   lst$cutoff <- cutoff
+  lst$true_imbalance <- sum(dataset$Y) / nrow(dataset)
 
   for (i in 1:length(lst$prediction_matrix)) {
     true_values <- factor(dataset$Y, levels = c(0, 1))
@@ -21,10 +23,31 @@ youdens_J <- function(model, dataset) {
     lst$sensitivity[[i]] <- sensitivity
     lst$specificity[[i]] <- specificity
     lst$Youdens_J[[i]] <- sensitivity + specificity - 1
+    lst$accuracy[[i]] <- (TP + TN) / (TP + TN + FP + FN)
+    lst$dist[[i]] <- abs(sensitivity - specificity)
   }
-  print(class(lst$Youdens_J))
-  lst$optimal_J <- max(unlist(lst$Youdens_J))
-  lst$optimal_cutoff <- lst$cutoff[which(lst$Youdens_J == lst$optimal_J)]
+
+  lst$dist <- unlist(lst$dist)
+  lst$accuracy <- unlist(lst$accuracy)
+  lst$sensitivity <- unlist(lst$sensitivity)
+  lst$specificity <- unlist(lst$specificity)
+  lst$Youdens_J <- unlist(lst$Youdens_J)
+  sortedJ <- sort(lst$Youdens_J, decreasing = TRUE)
+
+  done = FALSE
+  for (j in sortedJ) {
+    if (lst$dist[j == lst$Youdens_J] < 0.1) {
+      lst$optimal_J <- j
+      lst$optimal_cutoff <- lst$cutoff[j == lst$Youdens_J]
+      done = TRUE
+      break
+    }
+  }
+
+  if (!done) {
+    lst$optimal_J <- NULL
+  }
+
   return(lst)
 }
 
